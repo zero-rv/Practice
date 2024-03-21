2e3bded47e0fdff13a2fe745a0e38162a8d9ead8
Daijin <152014061+daijin1903@users.noreply.github.com>
2024-03-21 11:21:02 +0700

    Add files via upload

--- ktltlab1.cpp
+++ ktltlab1.cpp
@@ -1,12 +1,10 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <string>
-using namespace std;
+#include "mylib.h"
 
-void b10();
-void b11();
-void b12();
-void input(string s, int& n);
+void b6();
+void b7();
+void b9();
+void bonus();
+
 
 int main()
 {
@@ -16,14 +14,26 @@ back:
 	cin >> x;
 	switch (x)
 	{
+	case 6:
+		b6();
+		goto back;
+	case 7:
+		b7();
+		goto back;
+	case 9:
+		b9();
+		goto back;
 	case 10:
-		b10();
+
 		goto back;
 	case 11:
-		b11();
+
 		goto back;
 	case 12:
-		b12();
+
+		goto back;
+	case 100:
+		bonus();
 		goto back;
 	case 0:
 		break;
@@ -33,300 +43,116 @@ back:
 	return 0;
 }
 
-void input(string s, int& n)
+void b6()
 {
-	cout << s;
-	cin >> n;
-}
-
-void b10()
-{
-	int n, arr[10][10] = { 0 }, value = 1;
+	int n;
 	do
 	{
-		input("Nhap do dai mang nxn (2 <= n <= 10): ", n);
-	} while (n <= 2 || n >= 10);
-	int tmp = n - 1;
-	for (int j = 0; j < n; j++)
-	{
-		for (int i = 0; i < n; i++)
-		{
-			if (j == 0 || j % 2 == 0)
-			{
-				arr[i][j] = value;
-				value++;
-			}
-			if (j % 2 != 0)
-			{
-				arr[tmp - i][j] = value;
-				value++;
-			}
-		}
-	}
+		input("Nhap so phan tu cua mang (n>0): ", n);
+	} while (n <= 0);
+
+	int *arr = new int[n];
 
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
-		{
-			cout << arr[i][j] << " ";
-		}
-		cout << endl;
+		arr[i] = rand() % 100 + 1;
 	}
+	printArr(arr, n);
+	cout << endl;
+	reverseArr(arr, n);
+	cout << "Mang sau khi dao nguoc: ";
+	printArr(arr, n);
 	cout << endl;
 }
 
-void b11()
+void b7()
 {
-	int size, arr[10][10], value = 1, count = 0;
+	srand(time(NULL));
+	int n, x;
 	do
 	{
-		input("Nhap do dai mang nxn (2 <= n <= 10): ", size);
-	} while (size <= 2 || size >= 10);
-	int left = 0, right = size - 1, top = 0, bottom = size - 1;
-	//Matrix
-	while (true)
+		input("Nhap so phan tu cua mang (n>0): ", n);
+	} while (n <= 0);
+
+	int *arr = new int[n];
+
+	for (int i = 0; i < n; i++)
 	{
-		//Tren: trai -> phai
-		for (int j = left; j <= right; j++)
-		{
-			arr[top][j] = value;
-			value++;
-		}
-		top++;
-		if (top > bottom) { break; }
-
-		//Phai: tren -> duoi
-		for (int i = top; i <= bottom; i++)
-		{
-			arr[i][right] = value;
-			value++;
-		}
-		right--;
-		if (right < left) { break; }
-
-		//Duoi: phai -> trai
-		for (int j = right; j >= left; j--)
-		{
-			arr[bottom][j] = value;
-			value++;
-		}
-		bottom--;
-		if (bottom < top) { break; }
-
-		//Trai: duoi -> tren
-		for (int i = bottom; i >= top; i--)
-		{
-			arr[i][left] = value;
-			value++;
-		}
-		left++;
-		if (left > right) { break; }
+		arr[i] = rand() % 100 + 1;
 	}
-
-	//Print Array
-	for (int i = 0; i < size; i++)
-	{
-		for (int j = 0; j < size; j++)
-		{
-			cout << arr[i][j] << " ";
-		}
-		cout << endl;
-	}
+	printArr(arr, n);
 	cout << endl;
-}
+	input("Nhap gia tri can tim: ", x);
+	if (findIndexValue(arr, n, x) != new int(-1))
+		cout << "Tim thay tai phan tu arr[" << *findIndexValue(arr, n, x) << "]" << endl;
+	else
+		cout << "Khong tim thay gia tri can tim!" << endl;
 
-//POINTER ARRAY
-void addElement(string* &arr, int& size, string new_value)
-{
-	//Create new array using pointer
-	int new_size = size + 1;
-    string* new_array = new string[new_size];
-	//Copy current array to new array
-	for (int i = 0; i < size; i++)
-	{
-		new_array[i] = arr[i];
-	}
-	//Add a new element to the last index of new array
-	new_array[new_size - 1] = new_value;
-	//Assign new array to current array
 	delete[] arr;
-	arr = new_array;
-	//Assign new size to current size
-	size = new_size;
 }
 
-void b12()
+void b9()
 {
-	char seats[13][6];
-	int size = 0;
-	string *booked_tickets = new string[size];
-	int row = -1, col = -1;
-	for (int i = 0; i < 13; i++)
+	srand(time(NULL));
+	int r, c;
+	input("Nhap so hang: ", r);
+	input("Nhap so cot: ", c);
+	int** arr = new int*[r];
+	for (int i = 0; i < r; i++)
 	{
-		for (int j = 0; j < 6; j++)
+		arr[i] = new int[c];
+	}
+
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
 		{
-			seats[i][j] = 'X';
+			arr[i][j] = rand() % 100 + 1;
 		}
 	}
-	//MENU
-_menu1:
-	system("cls");
-	cout << "|--------- Dat ve may bay ---------|" << endl;
-	cout << "|1. Dat cho ngoi                   |" << endl;
-	cout << "|2. Xem cho ngoi                   |" << endl;
-	cout << "|3. Kiem tra ve                    |" << endl;
-	cout << "|                                  |" << endl;
-	cout << "|0. Thoat                          |" << endl;
-	cout << "|----------------------------------|" << endl;
-	cout << "Select: ";
-	int select;
-	cin >> select;
-	system("cls");
 
-	switch (select)
+	for (int i = 0; i < r; i++)
 	{
-	case 1: //Dat cho ngoi
-	_menu2:
-		system("cls");
-		cout << "|---------- Chon loai ve ----------|" << endl;
-		cout << "|1. Thuong gia                     |" << endl;
-		cout << "|2. Pho thong                      |" << endl;
-		cout << "|3. Tiet kiem                      |" << endl;
-		cout << "|                                  |" << endl;
-		cout << "|0. Thoat                          |" << endl;
-		cout << "|----------------------------------|" << endl;
-		cout << "Select: ";
-		cin >> select;
-		switch (select)
+		for (int j = 0; j < c; j++)
 		{
-		case 1:
-			cout << endl << "         A  B  C  D  E  F" << endl;
-			for (int i = 0; i < 2; i++)
-			{
-				if (i + 1 < 10)
-					cout << "Hang  " << i + 1 << "  ";
-				else
-					cout << "Hang " << i + 1 << "  ";
-
-				for (int j = 0; j < 6; j++)
-				{
-					cout << seats[i][j] << "  ";
-				}
-				cout << endl;
-			}
-			cout << endl;
-			break;
-		case 2:
-			cout << endl << "         A  B  C  D  E  F" << endl;
-			for (int i = 2; i < 7; i++)
-			{
-				if (i + 1 < 10)
-					cout << "Hang  " << i + 1 << "  ";
-				else
-					cout << "Hang " << i + 1 << "  ";
-
-				for (int j = 0; j < 6; j++)
-				{
-					cout << seats[i][j] << "  ";
-				}
-				cout << endl;
-			}
-			cout << endl;
-			break;
-		case 3:
-			cout << endl << "         A  B  C  D  E  F" << endl;
-			for (int i = 7; i < 13; i++)
-			{
-				if (i + 1 < 10)
-					cout << "Hang  " << i + 1 << "  ";
-				else
-					cout << "Hang " << i + 1 << "  ";
-
-				for (int j = 0; j < 6; j++)
-				{
-					cout << seats[i][j] << "  ";
-				}
-				cout << endl;
-			}
-			cout << endl;
-			break;
-		case 0:
-			goto _menu1;
-		default:
-			goto _menu2;
-		}
-
-	_input_seat_again:
-		cout << "Chon cho ngoi: " << endl;
-		cout << "_Chon cot (1 -> A ... 6 -> F): ";
-		cin >> col;
-		cout << "_Chon hang: ";
-		cin >> row;
-
-		switch (select)
-		{
-		case 1: //Dat cho ngoi
-			if (row < 1 || row > 2 || col < 1 || col > 6)
-			{
-				cout << "Cho ngoi khong hop le! Chon lai!" << endl;
-				goto _input_seat_again;
-			}
-			break;
-		case 2:
-			if (row < 3 || row > 7 || col < 1 || col > 6)
-			{
-				cout << "Cho ngoi khong hop le! Chon lai!" << endl;
-				goto _input_seat_again;
-			}
-			break;
-		case 3:
-			if (row < 8 || row > 13 || col < 1 || col > 6)
-			{
-				cout << "Cho ngoi khong hop le! Chon lai!" << endl;
-				goto _input_seat_again;
-			}
-			break;
-		}
-		seats[row - 1][col - 1] = 'R';
-		addElement(booked_tickets, size, (char)(col + 64) + to_string(row));
-		cout << "Dat cho thanh cong!" << endl;
-
-		system("pause");
-		goto _menu1;
-	case 2: //Xem cho ngoi
-		cout << "         A  B  C  D  E  F" << endl;
-		for (int i = 0; i < 13; i++)
-		{
-			if (i + 1 < 10)
-				cout << "Hang  " << i + 1 << "  ";
-			else
-				cout << "Hang " << i + 1 << "  ";
-
-			for (int j = 0; j < 6; j++)
-			{
-				cout << seats[i][j] << "  ";
-			}
-			cout << endl;
+			cout << arr[i][j];
+			if (j < c - 1)
+				cout << ", ";
 		}
 		cout << endl;
-		system("pause");
-		goto _menu1;
-	case 3:
-		if (size == 0)
-			cout << "Ban chua dat ve!" << endl;
-		else
-		{
-			for (int i = 0; i < size; i++)
-			{
-				cout << "Ve " << i + 1 << ": " << booked_tickets[i] << endl;
-			}
-		}
-		system("pause");
-		goto _menu1;
-	case 0:
-		return;
-	default:
-		goto _menu1;
 	}
 
+	int sum = cal_prime_value(arr, r, c, isPrime);
+
+	cout << "Tong cac so nguyen to trong mang: " << sum << endl;
+	delete[] arr;
+}
+
+void bonus()
+{
+	srand(time(NULL));
+
+	int n, x;
+	do
+	{
+		input("Nhap so phan tu cua mang (n>0): ", n);
+	} while (n <= 0);
+
+	int *arr = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100 + 1;
+	}
+
+	cout << "Mang ban dau: ";
+	printArr(arr, n);
+	cout << endl;
+	input("Nhap gia tri muon them: ", x);
+	pushBackArr(arr, n, x);
+	cout << "Mang sau khi them: ";
+	printArr(arr, n);
+	cout << endl;
+
+	delete[] arr;
 }
